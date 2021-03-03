62239e2aebca4c137b68e8070640c159597994cf
Mario Fink <mario.fink@record-evolution.de>
2021-03-03 18:25:17 +0100

    clean up pip/

--- pip/setup.py
+++ pip/setup.py
@@ -1,26 +1,13 @@
-# import setuptools
+
 from setuptools import setup, Extension
-# from distutils.core import setup
-# from distutils.extension import Extension
-# from Cython.Build import cythonize
+import os
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-# extensions = Extension(
-#     name="tdm_termite",
-#     sources=["py_tdm_termite.pyx","py_tdm_termite.cpp"],
-#     # libraries=[""],
-#     # library_dirs=["lib"],
-#     include_dirs=["lib","3rdparty/pugixml"],
-#     language='c++',
-#     extra_compile_args=['-std=c++17','-Wno-unused-variable'],
-#     extra_link_args=['-std=c++17'],
-# )
-
 setup(
     name="TDMtermite-RecordEvolution",
-    version="0.6.3",
+    version="0.6.7", #version,
     author="Record Evolution GmbH",
     author_email="mario.fink@record-evolution.de",
     maintainer="Record Evolution GmbH",
@@ -38,7 +25,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    # ext_modules=cythonize(extensions),
     ext_modules=[Extension("tdm_termite",
                            ["py_tdm_termite.cpp"],
                            # libraries_dirs=["cython/"],
@@ -48,20 +34,4 @@ setup(
                            extra_compile_args=['-std=c++17','-Wno-unused-variable'],
                            extra_link_args=['-std=c++17'],
                            )],
-    # packages=setuptools.find_packages(),
-    # python_requires=">=3.6",
 )
-
-# from distutils.core import setup, Extension
-#
-# moduleA = Extension('tdm_termite',
-#                     sources = ['cython/py_tdm_termite.cpp'])
-#
-# setup (name = 'TDMtermite',
-#        version = '0.5.4',
-#        description="Extract and read data from National Instruments LabVIEW tdx/tdm files and export them as csv files",
-#        long_description=long_description,
-#        url="https://github.com/RecordEvolution/TDMtermite.git",
-#        author="Record Evolution GmbH",
-#        author_email="mario.fink@record-evolution.de",
-#        ext_modules = [moduleA])
