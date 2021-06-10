6b7e0258b3bf8c44ddfea96d0af80b6b9733d1e6
Mario Fink <knif.oiram@gmail.com>
2021-06-10 22:30:20 +0200

    introduce platform dependent compile/link options for setup.py

--- cython/setup.py
+++ cython/setup.py
@@ -2,19 +2,9 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
 import os
+import sys
 
-extensions = Extension(
-    name="tdm_termite",
-    sources=["cython/py_tdm_termite.pyx"],
-    # libraries=[""],
-    # library_dirs=["lib"],
-    include_dirs=["lib","3rdparty/pugixml"],
-    language='c++',
-    #extra_compile_args=['-std=c++17','-Wno-unused-variable'],
-    extra_compile_args=['-std=c++17','/EHsc','/std:c++17'],
-    extra_link_args=['-std=c++17','/EHsc','/std:c++17'],
-)
-
+print("building on platform: "+sys.platform)
 os.system("git tag > gittags.log")
 with open ("gittags.log","r") as gt:
     taglst = gt.readlines()
@@ -24,6 +14,27 @@ if len(taglst) > 0 :
 else:
     version = 'unkown'
 print("building version: "+version)
+
+if sys.platform == "linux" or sys.platform == "darwin" :
+	cmpargs = ['-std=c++17','-Wno-unused-variable']
+	lnkargs = ['-std=c++17','-Wno-unused-variable']
+elif sys.platform == "win32" :
+	cmpargs = ['/EHsc','/std:c++17']
+	lnkargs = ['/EHsc','/std:c++17']
+else :
+	raise RuntimeError("unknown platform") 
+
+extensions = Extension(
+    name="tdm_termite",
+    sources=["cython/py_tdm_termite.pyx"],
+    # libraries=[""],
+    # library_dirs=["lib"],
+    include_dirs=["lib","3rdparty/pugixml"],
+    language='c++',
+    #extra_compile_args=['-std=c++17','-Wno-unused-variable'],
+    extra_compile_args= cmpargs,
+    extra_link_args= lnkargs,
+)
 
 setup(
     version=version,
